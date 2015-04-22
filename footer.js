8a0d533d35d9c9b0ae52083c463326ca0e4da0f8
Sarrgas <sargas1@hotmail.com>
2015-04-22 11:58:51 +0200

    Flyttat ut footer kod till jQuery

---
+++ footer.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('footer').html("\
+		<div class='button'><img src='themes/images/icons-png/star-white.png' class='buttonimg'></div> \
+		<div class='button'><a href='#lista'><img src='themes/images/icons-png/bullets-white.png' class='buttonimg'></a></div>\
+		<div class='button'><a href='#home'><img src='themes/images/icons-png/location-white.png' class='buttonimg'></a></div>\
+		<div class='button'><a href='#two'><img src='themes/images/icons-png/user-white.png' class='buttonimg'></a></div>");
+});
